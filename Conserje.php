3bdf1ec586fe88196297b7b6c0a9cd89f37d2231
elPulpo495 <pestala495@gmail.com>
2018-02-16 11:47:12 -0700

    Se agrego pantalla de video del conserje.

---
+++ Conserje.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Conserje</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css">
+</head>
+<body>
+	<center>
+		<table>
+			<tbody>
+				<td id="jan">
+					<img src="img/janitor.png" href="https://www.freepik.com/free-photos-vectors/truck">
+				</td>
+				<td>
+					<video controls> 
+						<source src="Mi bebe.mp4" type="video/mp4">
+					</video>
+				</td>
+				<td class="buttons">
+					<a href=""><img src="img/replay.png" alt="Circle with X in the middle."></a>
+					<br>
+					<a href=""><img src="img/exit.png" alt="Circle with an arrow in the middle."></a>
+				</td>
+			</tbody>
+		</table>
+	</center>
+</body>
+</html>
