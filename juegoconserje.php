99fa4a71adad10d052faa9c194cad5f60c0bdf4b
Cosme Escobedo <comeoes@gmail.com>
2018-02-19 09:20:09 -0700

    Puse la imagen del juego, lel

---
+++ juegoconserje.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html style="padding: 50px;">
+<head>
+    <title>Conserje</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body style="height: 100%; width: 100%;">
+    <center>
+    <div style="background-image: url('./img/juegoconserje.png'); background-size: cover;height: 525px; width: 934px;">
+    
+    </div>
+</center>
+
+</body>
+</html>
