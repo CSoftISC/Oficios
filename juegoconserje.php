9dcc6daf78fb6d472575cfd8beedd9101349377b
Yadira Peña <deathbat518@gmail.com>
2018-02-23 19:25:05 -0700

    juegito conserje seguuun

--- juegoconserje.php
+++ juegoconserje.php
@@ -4,12 +4,48 @@
     <title>Conserje</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+</body>
+<h1 align="center" style="position: absolute; left: 500px; top: -55px; width:120px; height:100px; font-size:80px"> Conserje </h1><
 <body style="height: 100%; width: 100%;">
-    <center>
-    <div style="background-image: url('./img/juegoconserje.png'); background-size: cover;height: 525px; width: 934px;">
+ 
+    </td> 
+            <td class="background">
+            	 <div style="position:absolute; left:50px; top:20px; width:10px; height:10px "> <img style=" width:350px; height:630px " src="img/est.png "></div>
+
+            	 <div style="position: absolute; left: 500px; top:20px; width: 10px; height: 10px "> <img style="width: 750px; height: 600px" src="img/OFFICINA.png"></div>
+
+            	 <div style="position: absolute; left: 700px; top:200px; width: 10px; height: 10px "> <img style="width: 250px; height: 450px" src="img/janitor.png"></div>
+            	</td>
+    </td>
+            <td class="Tools">
+               <div style="position: absolute; left: 50px; top: 280px; width: 10px; height: 10px "><img style="width: 100px; height: 350px " src="img/escoba.png"></div>        	
+               <div style="position: absolute; left: 8px; top: 280px; width: 10px; height: 10px "><img style="width: 100px; height: 360px " src="img/trapeador.png"></div>
+               <div style="position: absolute; left: 220px; top:450px; width: 10px; height: 10px "><img style="width: 180px; height: 180px " src="img/botee.png"></div>
+               <div style="position: absolute; left: 130px; top:500px; width: 10px; height: 10px "><img style="width: 100px; height: 100px " src="img/cepillo.png"></div>
+               <div style="position: absolute; left: 100px; top:350px; width: 10px; height: 10px "><img style="width: 100px; height: 100px " src="img/guantes.png"></div>
+               <div style="position: absolute; left: 180px; top:370px; width: 10px; height: 10px "><img style="width: 160px; height: 100px " src="img/trapo.png"></div>
+               <div style="position: absolute; left: 270px; top:340px; width: 10px; height: 10px "><img style="width: 130px; height: 100px " src="img/bote.png"></div>
+                <div style="position: absolute; left: 250px; top:180px; width: 10px; height: 10px "><img style="width: 100px; height: 120px " src="img/jabon.png"></div>
+                <div style="position: absolute; left: 80px; top:200px; width: 10px; height: 10px "><img style="width: 200px; height: 120px " src="img/colorito.png"></div>
+                <div style="position: absolute; left: 250px; top:100px; width: 10px; height: 10px "><img style="width: 180px; height: 100px " src="img/sarten.png"></div>
+                <div style="position: absolute; left: 130px; top:80px; width: 10px; height: 10px "><img style="width: 150px; height: 100px " src="img/12.png"></div>
+                <div style="position: absolute; left: 50px; top:125px; width: 10px; height: 10px "><img style="width: 100px; height: 60px " src="img/regador.png"></div>
+               </td>
+ </div>
+    </td>
+				<td class="buttons">
+					<a href=""> <div style="position: absolute; left: 1230px; top: 10px; width: 5px; height: 5px "> <img src="img/replay.png" alt="Circle with X in the middle." style="width: 100px; height: 100px "></a>
+					<br>
+					<a href=""><img src="img/exit.png" alt="Circle with an arrow in the middle." style="width: 100px; height: 100px "></a>
+					<a href=""><img src="img/signo.png" alt="Circle with a plus sign in the middle." style="width: 100px; height: 120px "></a>
+				</td>
+
+
+
+
     
     </div>
-</center>
+
 
 </body>
 </html>
