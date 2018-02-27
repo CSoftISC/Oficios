ee0b54e39f16aa750cc4692cbe292edc86309f8b
Damian Escobedo <damian050697@gmail.com>
2018-02-27 14:01:04 +0000

    Las herramientas del juego de carpintero se pueden arrastrar

--- juegocarpintero.php
+++ juegocarpintero.php
@@ -1,60 +1,58 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
-    <!-- <title>Carpintero</title> <h1 align="center"> Carpintero <BR clear="all"> </h1 --> 
-    	
+    <!-- <title>Carpintero</title> <h1 align="center"> Carpintero <BR clear="all"> </h1 -->   	
     <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="./js/drag.js"></script>
+ </head>
 
-   </head>
-
-</body>
-    		 <h1 align="center" style="position: absolute; left: 500px; top: -55px; width:120px; height:100px; font-size:80px"> Carpintero </h1><
+<h1 align="center" style="position: absolute; left: 500px; top: -55px; width:120px; height:100px; font-size:80px"> Carpintero </h1>
 
 <body style="height: 100%; width: 100%;">
-    <div style="background-image: url('./img/BK.png'); background-size: cover;height: 610px; width: 400px; "></div>
-    </td> 
-            <td class="Tools">
-    <div style="position:absolute; left:250px; top:30px; width:10px; height:10px "> <img style=" width:120px; height:100px " src="img/casco.png "></div>
+<div style="background-image: url('./img/BK.png'); background-size: cover;height: 610px; width: 400px; "></div>
+    
+    <!--Tools -->
+<div style="position:absolute; left:250px; top:30px;" class="draggable"> <img style=" width:120px; height:100px " src="img/casco.png "></div>
 
-     <div style="position:absolute; left:-5px; top:130px; width:10px; height:10px "> <img style=" width:150px; height:150px " src="img/herramienta.png "></div>
-     <div style="position:absolute; left:110px; top:130px; width:10px; height:10px "> <img style=" width:150px; height:150px " src="img/martillo.png "></div>
+<div style="position:absolute; left:-5px; top:130px;" class="draggable"> <img style=" width:150px; height:150px " src="img/herramienta.png "></div>
 
-       <div style="position:absolute; left:50px; top:26px; width:5px; height:5px "> <img style=" width:120px; height:120px " src="img/cinta.png "></div>
+<div style="position:absolute; left:110px; top:130px;" class="draggable"> <img style=" width:150px; height:150px " src="img/martillo.png "></div>
 
-         <div style="position:absolute; left:130px; top:270px; width:5px; height:5px "> <img style=" width:120px; height:120px " src="img/guantes.png "></div>
+<div style="position:absolute; left:50px; top:26px;" class="draggable"> <img style=" width:120px; height:120px " src="img/cinta.png "></div>
 
-           <div style="position:absolute; left:250px; top:150px; width:5px; height:5px "> <img style=" width:120px; height:120px " src="img/ll.png "></div>
+<div style="position:absolute; left:130px; top:270px;" class="draggable"> <img style=" width:120px; height:120px " src="img/guantes.png "></div>
 
-             <div style="position:absolute; left:280px; top:300px; width:5px; height:5px "> <img style=" width:120px; height:120px " src="img/regla.png "></div>
+<div style="position:absolute; left:250px; top:150px;" class="draggable"> <img style=" width:120px; height:120px " src="img/ll.png "></div>
 
-               <div style="position:absolute; left:1px; top:300px; width:5px; height:5px "> <img style=" width:120px; height:120px " src="img/clavos.png "></div>
+<div style="position:absolute; left:280px; top:300px;" class="draggable"> <img style=" width:120px; height:120px " src="img/regla.png "></div>
 
-                <div style="position:absolute; left:280px; top:450px; width:5px; height:5px "> <img style=" width:120px; height:120px " src="img/taladro.png "></div>
+<div style="position:absolute; left:1px; top:300px;" class="draggable"> <img style=" width:120px; height:120px " src="img/clavos.png "></div>
+
+<div style="position:absolute; left:280px; top:450px;" class="draggable"> <img style=" width:120px; height:120px" src="img/taladro.png "></div>
     		      
-    		      <div style="position:absolute; left:1px; top:480px; width:5px; height:5px "> <img style=" width:100px; height:100px " src="img/bote.png "></div>
+<div style="position:absolute; left:1px; top:480px;" class="draggable"> <img style=" width:100px; height:100px " src="img/bote.png "></div>
 
-    		       <div style="position:absolute; left:120px; top:450px; width:5px; height:5px "> <img style=" width:150px; height:150px " src="img/cer.png "></div>
-                  </td>
-           </td> 
-                   <td class="things">  
+<div style="position:absolute; left:120px; top:450px;" class="draggable"> <img style=" width:150px; height:150px " src="img/cer.png "></div>
 
-    		         <div style="position:absolute; left:450px; top:310px; width:5px; height:5px "> <img style=" width:600px; height:300px " src="img/escritorio.png "></div>
 
-    		         <div style="position: absolute; left: 900px; top:90px; width: 5px; height: 5px "> <img style=" width: 250px; height: 250px " src="img/st.png"></div>
+ <!--Things-->
+<div style="position:absolute; left:450px; top:310px; width:5px; height:5px "> <img style=" width:600px; height:300px " src="img/escritorio.png "></div>
 
-    		         <div style="position: absolute; left: 600px; top:100px; width: 5px; height: 5px "> <img style=" width: 200px; height: 200px " src="img/tab.png"></div>
+<div style="position: absolute; left: 900px; top:90px; width: 5px; height: 5px "> <img style=" width: 250px; height: 250px " src="img/st.png"></div>
+
+<div style="position: absolute; left: 600px; top:100px; width: 5px; height: 5px "> <img style=" width: 200px; height: 200px " src="img/tab.png"></div>
     
 <div style="position: absolute; left: 1040px; top:310px; width: 5px; height: 5px "> <img style=" width: 250px; height: 300px " src="img/carpinter.png"></div>
-            </td>
-   
     </div>
-    </td>
-				<td class="buttons">
-					<a href=""> <div style="position: absolute; left: 1180px; top: 10px; width: 5px; height: 5px "> <img src="img/replay.png" alt="Circle with X in the middle." style="width: 100px; height: 100px "></a>
-					<br>
-					<a href=""><img src="img/exit.png" alt="Circle with an arrow in the middle." style="width: 100px; height: 100px "></a>
-					<a href=""><img src="img/signo.png" alt="Circle with a plus sign in the middle." style="width: 100px; height: 120px "></a>
-				</td>
+   
+				  <!--Buttons-->
+<a href=""> <div style="position: absolute; left: 1180px; top: 10px; width: 5px; height: 5px "> <img src="img/replay.png" alt="Circle with X in the middle." style="width: 100px; height: 100px "></a>
+<br>
+<a href=""><img src="img/exit.png" alt="Circle with an arrow in the middle." style="width: 100px; height: 100px "></a>
+<a href=""><img src="img/signo.png" alt="Circle with a plus sign in the middle." style="width: 100px; height: 120px "></a>
+				
 
 </body>
 </html>
