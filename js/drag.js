8dbb382cc48d89d18347cba594a68b68d4a7aa27
Cosme Escobedo <comeoes@gmail.com>
2018-03-08 18:03:14 -0700

    Puse cosas para poder acomodar las herramientas
    -Ya se pueden acomodar las herramientas para el conserje.
    -En los otros oficios nada mas falta ponerle las coordenadas a las
    herramientas de donde quieras que se acomoden.
    -Puse las que se activen unas funciones cuando acertas o te equivoquas
    cuando pones las herrmientas pero todavia no hacen nada.

--- js/drag.js
+++ js/drag.js
@@ -1,5 +1,57 @@
+var startpos;
+
 $(document).ready(function(){
   $( function() {
-    $( ".draggable").draggable();
+    $( ".draggable").draggable({
+      start: function(event, ui){
+        startpos = ui.position
+      },
+      stop: stopDrag
+    });
   });
 });
+
+function stopDrag(event, ui) {
+  if (inContainer(ui.helper)) {
+    if (!ui.helper.attr("dropLeft")) {
+      mostrarError();
+      regresarPosOriginal(ui.helper);
+      return;
+    }
+    mostrarAcierto()
+    var left = ui.helper.attr("dropLeft");
+    var top = ui.helper.attr("dropTop");
+    ui.helper.animate({ "top": top, "left": left });
+  } else {
+    regresarPosOriginal(ui.helper)
+  }
+  
+}
+
+function regresarPosOriginal(elemento) {
+  elemento.animate({"top": startpos.top, "left": startpos.left})
+}
+
+function inContainer(elemento) {
+  var contenedor = $("#contenedor");
+  var cont_w = contenedor.width();
+  var cont_h = contenedor.height();
+  var cont_t = contenedor.position().top;
+  var cont_l = contenedor.position().left;
+  
+  var elemLeft = elemento.offset().left + elemento.width()/2;
+  var elemTop = elemento.offset().top+elemento.height()/2;
+
+  return (elemLeft > cont_l && elemLeft < cont_l+cont_w  && elemTop > cont_t && elemTop < cont_t+cont_h);
+  
+}
+
+function mostrarError() {
+  //TODO: hacer esta funcion lel
+  console.log("error")
+}
+
+function mostrarAcierto() {
+    //TODO: hacer esta funcion lel
+    console.log("acierto")
+}
