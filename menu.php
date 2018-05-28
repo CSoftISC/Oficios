dd5bcf2038efcb048c3fbb928534b44411866102
unknown <silveira.alberto24@gmail.com>
2018-05-28 10:41:13 -0600

    En menu.php se usa jquery local en lugar de internet

--- menu.php
+++ menu.php
@@ -69,7 +69,7 @@
 	</div>
 
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
 
 	<script type="text/javascript">
 		window.addEventListener("load", function(){
